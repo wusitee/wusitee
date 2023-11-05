72d8e9a902d5534d63098c690d9de4ab2726c12c
wusitee <kekdkkwk@outlook.com>
2023-11-05 11:28:18 +0800

    add my neovim settings

---
+++ nvim/lua/plugins/nvim-surround.lua
@@ -0,0 +1,7 @@
+return {
+    "kylechui/nvim-surround",
+    -- Use for stability; omit to use `main` branch for the latest features
+    version = "*",
+    event = "VeryLazy",
+    config = true,
+}

99dc7663d6bdeb44d2f89e8be845b71e068e74e3
wusitee <kekdkkwk@outlook.com>
2023-11-19 13:01:43 +0800

    更新powershell配置文件

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,13 @@
-Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardWord
 
 #set the color
 
 # enable z
+# Invoke-Expression (& { (lua C:/Users/Simon/Scoop/apps/z.lua/current/z.lua --init powershell enhanced) -join "`n" })
 
 Import-Module posh-git
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\robbyrussell.omp.json" | Invoke-Expression
 # Invoke-Expression (&starship init powershell)
 
-Invoke-Expression (& { (lua C:/Scoop/apps/z.lua/current/z.lua --init powershell) -join "`n" })
 
 Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
 
@@ -29,8 +28,16 @@ function ListDirectory {
 	(Get-ChildItem).Name
 	Write-Host("")
 }
-Set-Alias -Name ls -Value ListDirectory
+# Set-Alias -Name ls -Value ListDirectory
 Set-Alias -Name ll -Value Get-ChildItem
+
+# remove the powershell ls in order to use the ls from busy-box
+Remove-Alias -Name ls
+Remove-Alias -Name rm
+Remove-Alias -Name mv
+
+# # remove the alias of man in order to use the man from busy-box
+# Remove-Alias -Name man
 
 function OpenCurrentFolder {
 	param
@@ -40,3 +47,11 @@ function OpenCurrentFolder {
 	Invoke-Item $Path
 }
 Set-Alias -Name open -Value OpenCurrentFolder
+# Set-Alias -Name pip -Value pip3
+
+# 使用自动补全
+Import-Module Az.Tools.Predictor
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardWord
+# use zoxide for autojummp
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
